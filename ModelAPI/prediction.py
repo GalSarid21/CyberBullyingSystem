8bef4c26c1061c68bdee5b97ba2acb30e8e46500
GalSarid21 <galsarid@gmail.com>
2023-04-05 00:48:41 +0300

    fix prediction enum indices

--- ModelAPI/prediction.py
+++ ModelAPI/prediction.py
@@ -4,8 +4,8 @@ class PredValsIndex(IntEnum):
     TOXIC = 0,
     SEVERE_TOXIC = 1,
     OBSCENE = 2,
-    INSULT = 3,
-    THREAT = 4,
+    THREAT = 3,
+    INSULT = 4,
     IDENTITY_HATE = 5
 
 class Prediction():
@@ -16,8 +16,8 @@ class Prediction():
         self.toxic = True if pred_vals[0][PredValsIndex.TOXIC.value] > self.__threshold else False
         self.severe_toxic = True if pred_vals[0][PredValsIndex.SEVERE_TOXIC.value] > self.__threshold else False
         self.obscene = True if pred_vals[0][PredValsIndex.OBSCENE.value] > self.__threshold else False
-        self.insult = True if pred_vals[0][PredValsIndex.INSULT.value] > self.__threshold else False
         self.threat = True if pred_vals[0][PredValsIndex.THREAT.value] > self.__threshold else False
+        self.insult = True if pred_vals[0][PredValsIndex.INSULT.value] > self.__threshold else False
         self.identity_hate = True if pred_vals[0][PredValsIndex.IDENTITY_HATE.value] > self.__threshold else False
 
 class UserPrediction(Prediction):
